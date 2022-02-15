1708e182b8dc66be867921056980f0d52d3d7423
ElliotEriksson <elliot@serait.se>
2022-02-15 14:11:44 +0100

    Added character class

---
+++ src/resources.py
@@ -0,0 +1,13 @@
+from ast import Pass
+
+
+class Character:
+
+    def __init__(self, name, health, attack, armor):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
